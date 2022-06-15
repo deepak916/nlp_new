b694383c2b34a258d6cc725b05662f9e9fd3edfe
Deepak kumar singh <69434251+deepak916@users.noreply.github.com>
2022-06-15 06:24:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -347,7 +347,7 @@ q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
 
 if st.button('Find'):
-    query = helper.query_point_creator(q1,q2)
+    query = query_point_creator(q1,q2)
     result = model.predict(query)[0]
     
     if result:
